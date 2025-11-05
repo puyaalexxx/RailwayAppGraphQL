b0a09bab70ebb431fed9f09f93a8fa5eb9abee4a
Alex <puyaalexxx@gmail.com>
2025-11-05 16:24:45 +0200

    added hotchocolate and voyager dependecies

--- RailwayAppGraphQL/Program.cs
+++ RailwayAppGraphQL/Program.cs
@@ -1,3 +1,4 @@
+using HotChocolate.AspNetCore.Voyager;
 using Microsoft.EntityFrameworkCore;
 using RailwayAppGraphQL.Data;
 using RailwayAppGraphQL.Extensions;
@@ -9,24 +10,32 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("MariaDB");
 
-    options.UseMySql( connectionString, ServerVersion.AutoDetect(connectionString)
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)
     );
 });
+
+// Add GraphQL server
+builder.Services
+    .AddGraphQLServer();
+// .AddQueryType<Query>()
+// .AddProjections()   // optional: for IQueryable
+//  .AddFiltering()
+//  .AddSorting();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     // applying migrations automatically
     await app.ApplyMigrationAsync();
-}
 else
-{
     app.UseHsts();
-}
 
 app.UseHttpsRedirection();
+
+app.MapGraphQL();
+
+app.UseVoyager();
 
 app.MapGet("/", () => "Hello World!");
 
